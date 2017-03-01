9436b8caff06499318e27682df958120593d95de
github username <github email address>
2017-03-01 16:22:16 -0500

    o

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["fiction", "non-fiction"]}
+  validate :clickbait
+
+
+  private
+  def clickbait
+    clickbait =[
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i,
+  ]
+    if clickbait.none?{ |word| word.match(title)}
+      errors.add(:title, "must include clickbait")
+    end
+  end
+
 end
